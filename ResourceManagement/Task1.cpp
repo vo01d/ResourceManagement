6df5f7e655a0fde7bf2bebde81e390a89152aec0
vo01d <a.gurmana@gmail.com>
2024-03-14 00:23:03 +0200

    uncomment

--- ResourceManagement/Task1.cpp
+++ ResourceManagement/Task1.cpp
@@ -1,65 +1,65 @@
-//#include <iostream>
-//#include <Windows.h>
-//#include <stdexcept>
-//
-//class FileGuard {
-//public:
-//	FileGuard(LPCWSTR filename, DWORD desiredAccess, DWORD shareMode,
-//		LPSECURITY_ATTRIBUTES securityAttributes, DWORD creationDisposition,
-//		DWORD flagsAndAttributes, HANDLE templateFile) {
-//		m_fileHandle = CreateFile(filename, desiredAccess, shareMode, securityAttributes, creationDisposition, flagsAndAttributes, templateFile);
-//		if (m_fileHandle == INVALID_HANDLE_VALUE) {
-//			throw std::runtime_error("Failed to open file.");
-//		}
-//	}
-//
-//	FileGuard(const FileGuard&) noexcept = delete;
-//	FileGuard& operator=(const FileGuard&) noexcept = delete;
-//
-//	FileGuard(FileGuard&& right) noexcept : m_fileHandle(right.m_fileHandle) {
-//		right.m_fileHandle = INVALID_HANDLE_VALUE;
-//	}
-//
-//	FileGuard& operator=(FileGuard&& right) noexcept {
-//		if (this != &right) {
-//			destroy();
-//
-//			m_fileHandle = right.m_fileHandle;
-//			right.m_fileHandle = INVALID_HANDLE_VALUE;
-//		}
-//
-//		return *this;
-//	}
-//
-//	~FileGuard() noexcept {
-//		destroy();
-//	}
-//
-//	HANDLE get() const {
-//		return m_fileHandle;
-//	}
-//private:
-//	void destroy() {
-//		if (m_fileHandle != INVALID_HANDLE_VALUE) {
-//			CloseHandle(m_fileHandle);
-//		}
-//	}
-//	HANDLE m_fileHandle;
-//};
-//
-//int main() {
-//	try {
-//		FileGuard fileGuard(TEXT("text.txt"), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-//
-//		DWORD bytesWritten;
-//		const char* text = "Hello! How are you doing? I hope you're having a wonderful day filled with joy and success!";
-//		if (!WriteFile(fileGuard.get(), text, std::strlen(text), &bytesWritten, NULL)) {
-//			throw std::runtime_error("Failed to write to file");
-//		}
-//
-//		std::cout << "Data written to file successfully." << std::endl;
-//	}
-//	catch (const std::exception& ex) {
-//		std::cout << ex.what() << std::endl;
-//	}
-//}
+#include <iostream>
+#include <Windows.h>
+#include <stdexcept>
+
+class FileGuard {
+public:
+	FileGuard(LPCWSTR filename, DWORD desiredAccess, DWORD shareMode,
+		LPSECURITY_ATTRIBUTES securityAttributes, DWORD creationDisposition,
+		DWORD flagsAndAttributes, HANDLE templateFile) {
+		m_fileHandle = CreateFile(filename, desiredAccess, shareMode, securityAttributes, creationDisposition, flagsAndAttributes, templateFile);
+		if (m_fileHandle == INVALID_HANDLE_VALUE) {
+			throw std::runtime_error("Failed to open file.");
+		}
+	}
+
+	FileGuard(const FileGuard&) noexcept = delete;
+	FileGuard& operator=(const FileGuard&) noexcept = delete;
+
+	FileGuard(FileGuard&& right) noexcept : m_fileHandle(right.m_fileHandle) {
+		right.m_fileHandle = INVALID_HANDLE_VALUE;
+	}
+
+	FileGuard& operator=(FileGuard&& right) noexcept {
+		if (this != &right) {
+			destroy();
+
+			m_fileHandle = right.m_fileHandle;
+			right.m_fileHandle = INVALID_HANDLE_VALUE;
+		}
+
+		return *this;
+	}
+
+	~FileGuard() noexcept {
+		destroy();
+	}
+
+	HANDLE get() const {
+		return m_fileHandle;
+	}
+private:
+	void destroy() {
+		if (m_fileHandle != INVALID_HANDLE_VALUE) {
+			CloseHandle(m_fileHandle);
+		}
+	}
+	HANDLE m_fileHandle;
+};
+
+int main() {
+	try {
+		FileGuard fileGuard(TEXT("text.txt"), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+		DWORD bytesWritten;
+		const char* text = "Hello! How are you doing? I hope you're having a wonderful day filled with joy and success!";
+		if (!WriteFile(fileGuard.get(), text, std::strlen(text), &bytesWritten, NULL)) {
+			throw std::runtime_error("Failed to write to file");
+		}
+
+		std::cout << "Data written to file successfully." << std::endl;
+	}
+	catch (const std::exception& ex) {
+		std::cout << ex.what() << std::endl;
+	}
+}
--- ResourceManagement/Task2.cpp
+++ ResourceManagement/Task2.cpp
@@ -1,57 +1,57 @@
-//#include <iostream>
-//#include <memory>
-//#include <winsock2.h>
-//#include <ws2tcpip.h>
-//
-//void closeSocketFunction(SOCKET* ptr) {
-//    if (*ptr != INVALID_SOCKET) {
-//        closesocket(*ptr);
-//    }
-//    delete ptr;
-//}
-//
-//struct SocketDeleterFunctor {
-//    void operator()(SOCKET* ptr) const {
-//        if (*ptr != INVALID_SOCKET) {
-//            closesocket(*ptr);
-//        }
-//        delete ptr;
-//    }
-//};
-//
-//int main() {
-//    WSADATA wsaData;
-//    int wsaerr;
-//    WORD wVersionRequested = MAKEWORD(2, 2);
-//    wsaerr = WSAStartup(wVersionRequested, &wsaData);
-//    if (wsaerr != 0) {
-//        std::cout << "The Winsock dll not found" << std::endl;
-//        return 0;
-//    }
-//    else {
-//        std::cout << "The Winsock dll found!" << std::endl;
-//        std::cout << "The status: " << wsaData.szSystemStatus << std::endl;
-//    }
-//
-//    auto socketDeleterLambda = [](SOCKET* ptr) {
-//        if (*ptr != INVALID_SOCKET) {
-//            closesocket(*ptr);
-//        }
-//        delete ptr;
-//    };
-//
-//    std::unique_ptr<SOCKET, decltype(socketDeleterLambda)> socket1(new SOCKET(socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)), socketDeleterLambda);
-//    std::unique_ptr<SOCKET, void(*)(SOCKET* ptr)> socket2(new SOCKET(socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)), closeSocketFunction);
-//    std::unique_ptr<SOCKET, SocketDeleterFunctor> socket3(new SOCKET(socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)), SocketDeleterFunctor());
-//    if (*socket1 == INVALID_SOCKET || *socket2 == INVALID_SOCKET || *socket3 == INVALID_SOCKET) {
-//        std::cout << "Error with sockets" << WSAGetLastError() << std::endl;
-//        return 0;
-//    }
-//    else {
-//        std::cout << "sockets is OK!" << std::endl;
-//    }
-//
-//    // other actions with sockets...
-//
-//    return 0;
-//}
+#include <iostream>
+#include <memory>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+void closeSocketFunction(SOCKET* ptr) {
+    if (*ptr != INVALID_SOCKET) {
+        closesocket(*ptr);
+    }
+    delete ptr;
+}
+
+struct SocketDeleterFunctor {
+    void operator()(SOCKET* ptr) const {
+        if (*ptr != INVALID_SOCKET) {
+            closesocket(*ptr);
+        }
+        delete ptr;
+    }
+};
+
+int main() {
+    WSADATA wsaData;
+    int wsaerr;
+    WORD wVersionRequested = MAKEWORD(2, 2);
+    wsaerr = WSAStartup(wVersionRequested, &wsaData);
+    if (wsaerr != 0) {
+        std::cout << "The Winsock dll not found" << std::endl;
+        return 0;
+    }
+    else {
+        std::cout << "The Winsock dll found!" << std::endl;
+        std::cout << "The status: " << wsaData.szSystemStatus << std::endl;
+    }
+
+    auto socketDeleterLambda = [](SOCKET* ptr) {
+        if (*ptr != INVALID_SOCKET) {
+            closesocket(*ptr);
+        }
+        delete ptr;
+    };
+
+    std::unique_ptr<SOCKET, decltype(socketDeleterLambda)> socket1(new SOCKET(socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)), socketDeleterLambda);
+    std::unique_ptr<SOCKET, void(*)(SOCKET* ptr)> socket2(new SOCKET(socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)), closeSocketFunction);
+    std::unique_ptr<SOCKET, SocketDeleterFunctor> socket3(new SOCKET(socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)), SocketDeleterFunctor());
+    if (*socket1 == INVALID_SOCKET || *socket2 == INVALID_SOCKET || *socket3 == INVALID_SOCKET) {
+        std::cout << "Error with sockets" << WSAGetLastError() << std::endl;
+        return 0;
+    }
+    else {
+        std::cout << "sockets is OK!" << std::endl;
+    }
+
+    // other actions with sockets...
+
+    return 0;
+}
